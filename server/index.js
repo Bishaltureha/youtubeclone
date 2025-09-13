4ba379a786ef883258c883ce2afb93485eb56509
Bishaltureha <turehabishal@gmail.com>
2025-09-13 09:34:56 +0530

    added dotenv to server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/youtube_db";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(express.json());
